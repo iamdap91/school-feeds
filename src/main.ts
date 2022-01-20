5acd59c39292163a94997d16ecdb70ccee59bb49
sam3 <45986023+iamdap91@users.noreply.github.com>
2022-01-20 17:02:00 +0900

    Revert "feature/swagger 스웨거 패키지 추가 및 설정"

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const globalPrefix = 'api';
-  const documentPrefix = 'api-doc';
-  const port = 3000;
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix(globalPrefix);
-
-  const config = new DocumentBuilder().setTitle('classting-assignment').setVersion('1.0').build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api-doc', app, document);
-
-  await app.listen(port);
-
-  console.log(`api : http://localhost:${port}/${globalPrefix}  api-doc : http://localhost:${port}/${documentPrefix}`);
+  await app.listen(3000);
 }
 bootstrap();
