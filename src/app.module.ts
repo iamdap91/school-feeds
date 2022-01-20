6bb2cf78c3242c2ddb2038b77eb9f2a6b798c367
3sam3 <iamdap91@gmail.com>
2022-01-20 17:18:33 +0900

    feature/admin-controller 관리자 모듈 컨트롤러 서비스 생성

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ManagersModule } from './managers/managers.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [ManagersModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
