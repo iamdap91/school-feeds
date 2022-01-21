85eb1eaaf68834e88e194da40c2716f0615f5395
3sam3 <iamdap91@gmail.com>
2022-01-22 00:11:20 +0900

    feature/posts 포스트 생성 기능

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, Request, UseGuards } from '@nestjs/common';
+import { PostsService } from './posts.service';
+import { ManagerJwtAuthGuard } from '../auth/guards/manager-jwt-auth.guard';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { CreatePostDto } from './dto';
 
+@ApiTags('posts')
 @Controller('posts')
-export class PostsController {}
+export class PostsController {
+  constructor(private readonly postsService: PostsService) {}
+
+  @Post()
+  @UseGuards(ManagerJwtAuthGuard)
+  @ApiBearerAuth('JWT-auth')
+  async createPost(@Body() body: CreatePostDto) {
+    return await this.postsService.createPost(body);
+  }
+}
--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -1,4 +1,18 @@
+import { Repository } from 'typeorm';
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+
+import { PostEntity } from '../models/entities';
+import { CreatePostDto } from './dto';
 
 @Injectable()
-export class PostsService {}
+export class PostsService {
+  constructor(
+    @InjectRepository(PostEntity)
+    private postEntityRepository: Repository<PostEntity>
+  ) {}
+
+  async createPost({ schoolId, content }: CreatePostDto) {
+    return !!(await this.postEntityRepository.save({ schoolId, content }));
+  }
+}
